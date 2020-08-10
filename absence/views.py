ee9995c66473a642aa9cec0cdb51742091de6157
radiol00 <diego-00@o2.pl>
2020-08-10 15:31:10 +0200

    Restricted requests /absence/{pk} only for owner of absence with given {pk}

--- absence/views.py
+++ absence/views.py
@@ -12,15 +12,28 @@ from rest_framework.generics import ListAPIView
 class AbsenceAPIView(viewsets.ModelViewSet):
     serializer_class = AbsenceSerializer
     queryset = Absence.objects.all()
-    # permission_classes = []    
 
+    class isOwnerOrReject(permissions.BasePermission):
+        def has_permission(self, request, view):
+            pk = view.kwargs['pk']
+            try:
+                user_absence = Absence.objects.get(pk=pk)                
+            except:
+                return False
+
+            if user_absence.user.id == request.user.id:
+                return True
+            return False
+
+
+    # for normal user to get his absences, fetch get on /user/absence/
     def get_permissions(self):
         if self.action == 'list':
             permission_classes = [permissions.IsAdminUser, permissions.IsAuthenticated]
         else:
-            permission_classes = [permissions.IsAuthenticated]
+            permission_classes = [permissions.IsAuthenticated, self.isOwnerOrReject]
         return [permission() for permission in permission_classes]
-
+    
 
     def create(self, request):
         serializer = AbsenceSerializer(data=request.data)
--- ais_rest_backend/settings.py
+++ ais_rest_backend/settings.py
@@ -133,3 +133,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES' : ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
+
+
+# CODE ONLY FOR JWT TESTING
+# from datetime import timedelta
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+#     'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
+# }
